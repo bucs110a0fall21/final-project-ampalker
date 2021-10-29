f4b2ffdb145b46394a534a77f21fff866c164521
jalker1 <jalker1@binghamton.edu>
2021-10-29 14:43:49 -0400

    John Paul Alker commit

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #import your controller
+import pygame
+
 
 def main():
+    pygame.init()
+    team = {"lead": "John Paul Alker", "backend": "John Paul Alker", "frontend": "?"}
+    print("Software Lead is:", team["John Paul Alker"])
+    print("Backend is:", team["John Paul Alker"])
+    print("Frontend is:" , team["frontend"])
+main()
+
+#def main():
     #Create an instance on your controller object
     #Call your mainloop
     
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
-main()
+#main()
