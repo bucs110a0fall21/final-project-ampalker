7a3fab0d84ea3e3ee8ebdbca0b05e928a277d3cd
Paul <pampadu1@binghamton.edu>
2021-10-29 15:01:54 -0400

    Commit Paul

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 #import your controller
+import pygame
 
 def main():
-    #Create an instance on your controller object
-    #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
+	pygame.init()
+	team = {"lead":"?","backend":"John Paul Alker", "frontend":"Paul Ampadu"}
+	print("Software Lead is:", team["lead"])
+	print("Backend is:", team["backend"])
+	print("Frontend is:", team["frontend"])
+
+	#Create an instance on your controller object
+	#Call your mainloop
+	
+	###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
 main()
