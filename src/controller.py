823a330708f1c83d7f55479600b143a9653ecd84
pgrwe <aldreichampadu@outlook.com>
2021-12-04 19:12:33 -0500

    working on controller

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,5 @@
-import pygame 
+import os 
+import pygame
 import sys
 from src import character
 #from src import enemy
@@ -9,38 +10,60 @@ class Controller:
     clock = pygame.time.Clock()
     def __init__(self):
         pygame.init()
-        self.state = "MENU"
-        self.window = (500,500)
-        self.screen = pygame.display.set_mode(self.window)
-        self.background = pygame.Surface(self.window)
-        self.background.fill((250,250,250))
+        self.width = 900
+        self.height = 500
+        self.screen = pygame.display.set_mode(self.width,self.height)
         self.vagabond = character.Hero(7,10,"fortemps",'assets/Vagabond.png',10,4)
+        self.state = "MENU"
        # self.enemy = enemy.Enemy()
 
-    def mainLoop(self):
+    def mainloop(self):
         while self.state:
             if self.state == "MENU":
                 self.menuloop()
             elif self.state == "GAME":
                 self.gameloop()
-            #elif self.state == "GAMEOVER":
-               # self.endloop()
+            elif self.state == "GAMEOVER":
+                self.endloop()
+            elif self.state == "EXIT":
+                self.exitloop()
     
+    def button(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+    
+    def render(self):
+        self.screen.blit(self.image, (self.rect.x, self.rect.y))
+
     def menuloop(self):
         """
         menu code
         """
-        while self.state == "MENU":
-            pygame.image.load('assets/Vagrant Title PNG.png')
+        self.title_img = pygame.image.load(os.path.join('assets', 'Vagrant Title.png'))
+        self.start_button_img = pygame.image.load(os.path.join('assets', 'Vagrant Start.png'))
+        self.exit_button_img = pygame.image.load(os.path.join('assets', 'Vagrant Exit.png'))
+        run = True
+        while run:
+            #self.screen.fill(49,50,51)
+            self.start_button = self.button(250,300,self.start_button_img)
+            self.render()
 
+            for self.event in pygame.event.get():
+                if self.event.type == pygame.QUIT:
+                    run = False
+        self.exitloop()
 
 
     def gameloop(self):
         for self.event in pygame.event.get():
             if self.event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                self.exitloop()
         pygame.display.update()
+
+    def exitloop(self):
+        pygame.quit()
+        sys.exit()
 
 
     def keyboard(self):
@@ -63,6 +86,7 @@ class Controller:
                     self.move_left == False
                 if event.key == K_RETURN:
                     self.start_key = False
+                
 
 
 
