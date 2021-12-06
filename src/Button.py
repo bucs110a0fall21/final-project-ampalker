f86e11166c757b5e33207cbee5876b3f1587deac
pgrwe <aldreichampadu@outlook.com>
2021-12-06 18:52:52 -0500

    menu working

--- src/Button.py
+++ src/Button.py
@@ -8,10 +8,30 @@ class MakeButton:
         self.image = pygame.transform.scale(image, (int(width*scale), int(height*scale)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
+        self.click = False
     
     def render(self,screen_obj):
         screen_obj.blit(self.image, (self.rect.x, self.rect.y))
+    
+    def start_button(self, mouse_pos):
+        event = False
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
+                self.click = True
+                event = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.click = False
+        return event
 
+    def exit_button(self, mouse_pos):
+        event = False
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
+                self.click = True
+                event = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.click = False
+        return event
 
 
 
--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,6 @@
 import os, pygame, sys
+
+from pygame import mouse
 from src import Character, Button #Enemy
 from pygame.constants import K_LEFT, K_RETURN, K_RIGHT, KEYDOWN, KEYUP
 
@@ -13,64 +15,44 @@ class Controller:
         self.screen = pygame.display.set_mode(self.size)
         self.screen.fill(white)
         self.vagabond = Character.Hero(7,10,"fortemps",'assets/Vagabond.png',10,4)
-        self.state = "GAME"
+        self.title_img = pygame.image.load(os.path.join('assets', 'Vagrant Title.png'))
+        self.start_button_img = pygame.image.load(os.path.join('assets', 'Vagrant Start.png')) 
+        self.exit_button_img = pygame.image.load(os.path.join('assets', 'Vagrant Exit.png'))
        # self.enemy = enemy.Enemy()
-
+        
     def mainloop(self):
-        while self.state:
-            if self.state == "MENU":
-                self.menuloop()
-            elif self.state == "GAME":
-                self.gameloop()
-            elif self.state == "GAMEOVER":
-                self.endloop()
-            elif self.state == "EXIT":
-                self.exitloop()
-    
-    
-    def menuloop(self):
         """
         menu code
         """
-        self.title_img = pygame.image.load(os.path.join('assets', 'Vagrant Title.png'))
+        
         self.title_img = self.title_img.convert_alpha()
-        self.title = Button.MakeButton(150,25,self.title_img,1.5)
-        self.start_button_img = pygame.image.load(os.path.join('assets', 'Vagrant Start.png')) 
+        self.title = Button.MakeButton(195,25,self.title_img,1.5)  
         self.start_button_img = self.start_button_img.convert_alpha()
-        self.start_button = Button.MakeButton(225,110,self.start_button_img,1)
-        self.exit_button_img = pygame.image.load(os.path.join('assets', 'Vagrant Exit.png'))
+        self.start_button = Button.MakeButton(320, 350,self.start_button_img,1)
         self.exit_button_img = self.exit_button_img.convert_alpha()
-        self.exit_button = Button.MakeButton(225,130,self.exit_button_img,1)
+        self.exit_button = Button.MakeButton(360,450,self.exit_button_img,1)
         run = True
-        click = False
         while run:
             self.screen.fill((49,50,51))
             self.title.render(self.screen)
             self.start_button.render(self.screen)
             self.exit_button.render(self.screen)
             mouse_pos = pygame.mouse.get_pos()
-            start_button_rect = pygame.Rect(225,110,40,30)
-            exit_button_rect = pygame.Rect(225,130,30,20)
-            pygame.draw.rect(self.screen, (250,250,250), start_button_rect)
-            pygame.draw.rect(self.screen, (250,250,250), exit_button_rect)
-            if start_button_rect.collidepoint(mouse_pos):
-                if click:
-                    print("button1")
-                    self.state == "GAME"
-            if exit_button_rect.collidepoint(mouse_pos):
-                if click:
-                    print("button2")
-                    self.state == "EXIT"
+            if self.start_button.start_button(mouse_pos):
+                print("start button")
+                self.gameloop()
+            if self.exit_button.exit_button(mouse_pos):
+                print("exit button")
+                self.exitloop()
             pygame.display.update()
             for self.event in pygame.event.get():
-                if self.event.type == pygame.mouse.get_pressed()[0] == 1:
-                    click = True
                 if self.event.type == pygame.QUIT:
                     run = False
         self.exitloop()
 
 
     def gameloop(self):
+        self.screen.fill((250,250,250))
         for self.event in pygame.event.get():
             if self.event.type == pygame.QUIT:
                 self.exitloop()
