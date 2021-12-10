61d69df2bc23095217fd4300d8ef9e4dfbbca6c9
Paul <pampadu1@binghamton.edu>
2021-12-09 20:59:08 -0500

    updated level design

--- src/Constants.py
+++ src/Constants.py
@@ -2,17 +2,17 @@
 Constants
 """
 level_map = [
-'X                 X     X',
-'X                 X     X', 
-'X                 X     X',
-'X                 X     X',
-'X                 X     X',
-'X                 X     X',
-'X                 X     X',
-'X                 X     X',
-'X                 X     X',
-'X                 X     X',
-'XXXXXXXXXXXXXXXXXXX     X']
+'                        X',
+'     X                  X', 
+'   XXX                  X',
+'            XXX         X',
+'           XXXX         X',
+'                        X',
+'                        X',
+'                 XX     X',
+'                 XX     X',
+'     XXXX       XXX     X',
+'    XXXXX    XXXXXX     X']
 tile_size = 64
 screen_width = 1200
 screen_height = len(level_map) * 64
